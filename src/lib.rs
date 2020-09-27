0bf9fa5abd9158612ddb1b3f892f02085a8f3049
Mark Gross <mark.gross2001@gmail.com>
2020-09-27 16:25:26 -0700

    Add rudimentary MMS parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use std::{error::Error, fmt};
 
+use base64;
 use xml::attribute::OwnedAttribute;
 
 #[derive(Debug)]
@@ -23,11 +24,11 @@ fn strip_chars(src: &str, chars: &str) -> String {
 
 #[derive(Debug)]
 pub struct Sms {
-    timestamp: u64,
-    outgoing: bool,
-    address: String,
-    contact: Option<String>,
-    body: String
+    pub timestamp: u64,
+    pub outgoing: bool,
+    pub address: String,
+    pub contact: Option<String>,
+    pub body: String
 }
 
 impl Sms {
@@ -57,5 +58,72 @@ impl Sms {
         }
 
         Ok(result)
+    }
+}
+
+#[derive(Debug)]
+pub struct Mms {
+    pub timestamp: u64,
+    pub addresses: Vec<String>,
+    pub parts: Vec<Part>
+}
+
+#[derive(Debug)]
+pub struct Part {
+    pub content_type: String,
+    pub text: Option<String>,
+    pub data: Option<Vec<u8>>
+}
+
+impl Mms {
+    pub fn blank() -> Mms {
+        Mms {
+            timestamp: 0,
+            addresses: Vec::new(),
+            parts: Vec::new()
+        }
+    }
+
+    pub fn new(attributes: &[OwnedAttribute]) -> Result<Mms, Box<dyn Error>> {
+        let mut result = Mms { 
+            timestamp: 0,
+            addresses: Vec::new(),
+            parts: Vec::new(),
+        };
+        for a in attributes {
+            match a.name.local_name.as_str() {
+                "date" => result.timestamp = a.value.parse::<u64>()?,
+                _ => ()
+            }
+        }
+        Ok(result)
+    }
+
+    pub fn add_addr(&mut self, attributes: &[OwnedAttribute]) -> Result<(), Box<dyn Error>> {
+        for a in attributes {
+            match a.name.local_name.as_str() {
+                "address" => self.addresses.push(a.value.clone()),
+                _ => ()
+            }
+        }
+        Ok(())
+    }
+
+    pub fn add_part(&mut self, attributes: &[OwnedAttribute]) -> Result<(), Box<dyn Error>> {
+        let mut part = Part { 
+            content_type: "".to_string(),
+            text: None,
+            data: None
+        };
+        for a in attributes {
+            match a.name.local_name.as_str() {
+                "ct" => part.content_type = a.value.clone(),
+                "text" => part.text = if a.value == "null" { None } else { Some(a.value.clone()) },
+                "data" => part.data = Some(base64::decode(&a.value)?),
+                _ => ()
+            }
+        }
+        self.parts.push(part);
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,49 @@ use std::io::BufReader;
 
 use xml::reader::{EventReader, XmlEvent};
 
-use sms_parser::Sms;
+use sms_parser::{Sms, Mms};
 
 fn main() {
     let file = File::open("sms.xml").unwrap();
     let file = BufReader::new(file);
 
     let parser = EventReader::new(file);
+    let mut mms = Mms::blank();
     for e in parser {
         match e {
             Ok(XmlEvent::StartElement { name, attributes, .. }) => {
-                if name.local_name != "sms" {
-                    continue;
+                match name.local_name.as_str() {
+                    "sms" => {
+                        // let sms = Sms::new(&attributes);
+                        // println!("{:?}", sms);
+                    },
+                    "mms" => {
+                        mms = Mms::new(&attributes).unwrap();
+                    },
+                    "part" => {
+                        mms.add_part(&attributes);
+                    },
+                    "addr" => {
+                        mms.add_addr(&attributes);
+                    },
+                    _ => ()
                 }
-
-                let sms = Sms::new(&attributes);
-                println!("{:?}", sms);
             }
-            // Ok(XmlEvent::EndElement { name }) => {
-            // }
+            Ok(XmlEvent::EndElement { name }) => {
+                if name.local_name == "mms" {
+                    print!("{} ", mms.timestamp);
+                    for a in mms.addresses {
+                        print!("{} ", a);
+                    }
+                    println!();
+                    for p in mms.parts {
+                        if p.content_type == "text/plain" {
+                            println!("{:?}", p);
+                        }
+                    }
+                    mms = Mms::blank();
+                }
+            }
             Err(e) => {
                 println!("Error: {}", e);
                 break;
